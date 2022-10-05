639c073c67710794e2cf1d2028ebd54672ebaca4
Sarah Sá <98770963+sarahalvessa@users.noreply.github.com>
2022-10-05 12:16:32 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+ function menuside(){
+ let menuList = document.querySelector(".menu-open");
+
+ menuList.style.marginTop == "-500px" || menuList.style.marginTop == "" ? menuList.style.marginTop = "0px" : menuList.style.marginTop = "-500px";
+
+ };
